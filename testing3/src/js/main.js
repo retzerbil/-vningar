3de972e0b087d56d04d526ed0626bdc9a3356c64
Andreas Retzius <andreasretzius@gmail.com>
2023-11-15 09:53:22 +0100

    testing stuff

--- testing3/src/js/main.js
+++ testing3/src/js/main.js
@@ -1,3 +1,4 @@
+const bodyTag = document.getElementById("body");
 const Cat ={name:"meow",color:"black"};
 
 localStorage.setItem("1","halo :))");
@@ -11,3 +12,17 @@ console.log(localStorage.getItem("1"));
 console.log(localStorage.getItem("coolestCat"));
 
 console.log(localStorage.getItem("test"));
+
+const lalala = localStorage.getItem("test");
+const lalala2 = JSON.parse(lalala);
+console.log(lalala2);
+
+const container = document.createElement("section");
+
+const button1 = document.createElement("button");
+container.appendChild(button1);
+
+button1.addEventListener("click", () => {
+    console.log("alo :)))))");
+});
+bodyTag.appendChild(container);
